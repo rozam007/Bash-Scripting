095d352549efb1f9e4607d0f26ef79900b617cee
rozam007 <rozam0040@gmail.com>
2025-03-13 16:29:46 +0500

    variabls

---
+++ scripts/03_variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# How to use variables
+
+name=rozam
+
+# This is a constant variable
+readonly age=27
+
+city=gujranwala
+
+echo "my name is $name, age is $age and i lives in $city"
+
+city=lahore
+
+echo "my name is $name, age is $age and i lives in $city"
+
+whichBash=$(basename "$SHELL")
+
+echo "I am using $whichBash"
+
